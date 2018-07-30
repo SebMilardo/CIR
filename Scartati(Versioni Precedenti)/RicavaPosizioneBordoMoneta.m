a90d8ff58c168e928dd7fd0c0e6d6a93f70f4448
Sebastiano Milardo <s.milardo@hotmail.it>
2018-07-30 00:01:00 -0400

    Final commit

--- Scartati(Versioni Precedenti)/RicavaPosizioneBordoMoneta.m
+++
@@ -1,7 +0,0 @@
-function [mb]=RicavaPosizioneBordoMoneta(datimoneta)
-    %Dato il box contenente la moneta ricaviamo un
-    %opportuno punto per ricavare il colore medio
-    %di una parte pi� esterna della moneta
-    
-    
-end
